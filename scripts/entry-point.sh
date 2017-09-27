0956967ee26d6475132c40a8873b994b465eb514
rajkumar <rajkumar@localhost.localdomain>
2017-09-27 19:06:53 -0400

    update to use default gensis Json

--- scripts/entry-point.sh
+++ scripts/entry-point.sh
@@ -1,2 +1,2 @@
-geth --datadir /home/ubuntu/eth-dev init /home/ubuntu/eth-dev/$1 
+geth --datadir /home/ubuntu/eth-dev init /usr/local/config/genesis.json 
 geth --networkid 45634 --verbosity 4  --ipcdisable --rpc --port 30301 --rpcport 8545 --rpcaddr 0.0.0.0 console 2>> /home/ubuntu/eth-dev/eth.log
